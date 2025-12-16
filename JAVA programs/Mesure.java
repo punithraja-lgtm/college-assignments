1b89017d0adf5dda5444879508ff76aa5d601267
punithraja-lgtm <punithrajapunithraj@gmail.com>
2025-12-16 20:34:14 +0530

    Add files via upload

---
+++ JAVA programs/Mesure.java
@@ -0,0 +1,50 @@
+abstract class formula{
+    abstract double speed();
+    abstract double velocity();
+}
+
+class Car extends formula{
+    private double distance;
+    private double time;
+    private double displacement;
+
+    public Car(double distance, double time, double displacement){
+        this.distance = distance;
+        this.time = time;
+        this.displacement = displacement;
+    }
+    double speed(){
+        return distance / time;
+    }
+    double velocity(){
+        return displacement / time;
+    }
+}
+class Bike extends formula{
+    private double distance;
+    private double time;
+    private double displacement;
+
+    public Bike(double distance, double time, double displacement){
+        this.distance = distance;
+        this.time = time;
+        this.displacement = displacement;
+    }
+    double speed(){
+        return distance / time;
+    }
+    double velocity(){
+        return displacement / time;
+    }
+}
+public class Mesure{
+    public static void main(String[] args) {
+        Car car = new Car(150, 3, 120);
+        Bike bike = new Bike(90, 2, 80);
+
+        System.out.println("Car Speed: " + car.speed() + " kmph");
+        System.out.println("Car Velocity: " + car.velocity() + " kmph");
+        System.out.println("Bike Speed: " + bike.speed() + " kmph");
+        System.out.println("Bike Velocity: " + bike.velocity() + " kmph");
+    }
+}
